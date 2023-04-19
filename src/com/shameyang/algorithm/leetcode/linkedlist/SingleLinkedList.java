93c6f96559bbe181d36bfc0dea1586effbf9b95c
shameyang <shameyang@outlook.com>
2023-04-19 21:20:28 +0800

    update

---
+++ src/com/shameyang/algorithm/leetcode/linkedlist/SingleLinkedList.java
@@ -0,0 +1,87 @@
+package com.shameyang.algorithm.leetcode.linkedlist;
+
+/**
+ * @author ShameYang
+ * @date 2023/4/19 19:55
+ * @description 设计单链表
+ */
+public class SingleLinkedList {
+    int size;
+    Node head;
+
+    public SingleLinkedList() {
+        size = 0;
+        head = new Node(0);
+    }
+
+    //头插
+    public void addAtHead(int val) {
+        Node node = new Node(val);
+        node.next = head.next;
+        head.next = node;
+        size++;
+    }
+
+    //尾插
+    public void addAtTail(int val) {
+        Node cur = head;
+        for (int i = 0; i < size; i++) {
+            cur = cur.next;
+        }
+        Node node = new Node(val);
+        cur.next = node;
+        size++;
+    }
+
+    //增加
+    public void addAtIndex(int index, int val) {
+        if (index > size) {
+            return;
+        }
+        if (index < 0) {
+            index = 0;
+        }
+        Node cur = head;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        Node node = new Node(val);
+        node.next = cur.next;
+        cur.next = node;
+        size++;
+    }
+
+    //删除
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
+        Node cur = head;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.next = cur.next.next;
+        size--;
+    }
+
+    //查找
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+        Node cur = head;
+        for (int i = 0; i <= index; i++) {
+            cur = cur.next;
+        }
+        return cur.val;
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+
+    public Node(int val) {
+        this.val = val;
+    }
+}
