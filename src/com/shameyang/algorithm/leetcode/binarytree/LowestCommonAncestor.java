1425015518fa061b2472313a5b6db43e97ed39f8
shameyang <shameyang@outlook.com>
2023-05-27 15:58:30 +0800

    update

---
+++ src/com/shameyang/algorithm/leetcode/binarytree/LowestCommonAncestor.java
@@ -0,0 +1,23 @@
+package com.shameyang.algorithm.leetcode.binarytree;
+
+/**
+ * @author ShameYang
+ * @date 2023/5/27 15:54
+ * @description 二叉树的最近公共祖先
+ */
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+        return root;
+    }
+}
