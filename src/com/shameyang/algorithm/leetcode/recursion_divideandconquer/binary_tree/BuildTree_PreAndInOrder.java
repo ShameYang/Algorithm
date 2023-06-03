3deda4bdedf2f6b6889f44b49117a79489883c19
shameyang <shameyang@outlook.com>
2023-06-03 18:38:21 +0800

    update

---
+++ src/com/shameyang/algorithm/leetcode/recursion_divideandconquer/binary_tree/BuildTree_PreAndInOrder.java
@@ -0,0 +1,44 @@
+package com.shameyang.algorithm.leetcode.recursion_divideandconquer.binary_tree;
+
+import com.shameyang.algorithm.leetcode.binary_tree.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ShameYang
+ * @date 2023/6/3 18:05
+ * @description 从前序与中序遍历序列构造二叉树
+ */
+public class BuildTree_PreAndInOrder {
+    Map<Integer, Integer> map = new HashMap<>();
+
+    public com.shameyang.algorithm.leetcode.binary_tree.TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = inorder.length;
+        for (int i = 0; i < n; i++) {
+            map.put(inorder[i], i);
+        }
+        return helper(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+
+    public TreeNode helper(int[] preorder, int[] inorder, int pre_left, int pre_right, int in_left, int in_right) {
+        if (pre_left > pre_right || in_left > in_right) {
+            return null;
+        }
+        //根节点的值
+        int rootVal = preorder[pre_left];
+        //在中序中定位根节点
+        int in_idx = map.get(rootVal);
+        //先构建根节点
+        TreeNode root = new TreeNode(rootVal);
+        //左子树的节点数目
+        int size_left_subtree = in_idx - in_left;
+
+        root.left = helper(preorder, inorder,
+                pre_left + 1, pre_left + size_left_subtree, in_left, in_idx - 1);
+        root.right = helper(preorder, inorder,
+                pre_left + size_left_subtree + 1, pre_right, in_idx + 1, in_right);
+
+        return root;
+    }
+}
---
+++ src/com/shameyang/algorithm/leetcode/recursion_divideandconquer/binary_tree/TreeNode.java
@@ -0,0 +1,25 @@
+package com.shameyang.algorithm.leetcode.recursion_divideandconquer.binary_tree;
+
+/**
+ * @author ShameYang
+ * @date 2023/6/3 18:15
+ * @description
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
